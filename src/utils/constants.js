089012adb6c71683115a516410f96faeaab13e6d
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-31 19:09:29 +0530

    export file for assets & some demo data

---
+++ src/utils/constants.js
@@ -0,0 +1,48 @@
+import MusicNoteIcon from "../assets/music.svg";
+import HomeIcon from "../assets/home.svg";
+import CodeIcon from "../assets/code.svg";
+import MovieIcon from "../assets/movie.svg";
+import GameIcon from "../assets/game.svg";
+import LiveTvIcon from "../assets/live.svg";
+import EducationIcon from "../assets/education.svg";
+import BeautyIcon from "../assets/beauty.svg";
+import FashionIcon from "../assets/fashion.svg";
+import PodcastIcon from "../assets/podcast.svg";
+import TheaterComedyIcon from "../assets/comedy.svg";
+import GymIcon from "../assets/gym.svg";
+import SportIcon from "../assets/sport.svg";
+import CryptoIcon from "../assets/crypto.svg";
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
+export const demoVideoUrl = "/video/GDa8kZLNhJ4";
+export const demoChannelTitle = "JavaScript Mastery";
+export const demoVideoTitle =
+	"Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
+export const demoProfilePicture =
+	"http://dergipark.org.tr/assets/app/images/buddy_sample.png";
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+	{ name: "New", icon: HomeIcon },
+	{ name: "JS Mastery", icon: CodeIcon },
+	{ name: "Hitesh Choudhary", icon: CodeIcon },
+	{ name: "Chai aur Code", icon: CodeIcon },
+	{ name: "Fireship", icon: CodeIcon },
+	{ name: "Coding", icon: CodeIcon },
+	{ name: "JavaScript", icon: CodeIcon },
+	{ name: "ReactJS", icon: CodeIcon },
+	{ name: "NextJS", icon: CodeIcon },
+	{ name: "Music", icon: MusicNoteIcon },
+	{ name: "Education", icon: EducationIcon },
+	{ name: "Podcast", icon: PodcastIcon },
+	{ name: "Movie", icon: MovieIcon },
+	{ name: "Gaming", icon: GameIcon },
+	{ name: "Live", icon: LiveTvIcon },
+	{ name: "Sport", icon: SportIcon },
+	{ name: "Fashion", icon: FashionIcon },
+	{ name: "Beauty", icon: BeautyIcon },
+	{ name: "Comedy", icon: TheaterComedyIcon },
+	{ name: "Gym", icon: GymIcon },
+	{ name: "Crypto", icon: CryptoIcon },
+];
