160cd68e6e1406fe6e0394e3897362d91f3e4256
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-02 12:59:05 +0530

    export file of components

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import VideoDetail from "./VideoDetail";
+import ChannelDetail from "./ChannelDetail";
+import SearchFeed from "./SearchFeed";
+import SearchBar from "./SearchBar";
+import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+import VideoCard from "./VideoCard";
+import ChannelCard from "./ChannelCard";
+
+export {
+	Navbar,
+	Feed,
+	VideoDetail,
+	ChannelDetail,
+	SearchFeed,
+	SearchBar,
+	Sidebar,
+	Videos,
+	VideoCard,
+	ChannelCard,
+};
